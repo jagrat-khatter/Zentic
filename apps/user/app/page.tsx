a2a86f3ed6032349b674969692fb48a0641afdf1
jagrat-khatter <jagratkhatter@gmail.com>
2026-02-22 14:17:10 +0000

    Setup Tailwind CSS in turborepo

---
+++ apps/user/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@repo/ui/button";
+
+
+export default function Home() {
+  return (
+    <>
+      <Button children={<div>HI there</div>}></Button>
+      <div className="text-2xl">Hey there </div>
+    </>
+  );
+}
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,20 +1,9 @@
-"use client";
+import * as React from "react";
 
-import { ReactNode } from "react";
-
-interface ButtonProps {
-  children: ReactNode;
-  className?: string;
-  appName: string;
-}
-
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export function Button({ children }: { children: React.ReactNode }) {
   return (
-    <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
-    >
+    <button className="bg-blue-200 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
       {children}
     </button>
   );
-};
+}
